48ad807224df971f2174b0b1eba230dbdda76cd3
MIHAI POPESCU <mihai.ppscu@gmail.com>
2017-02-20 07:36:20 +0200

    feat(sectionImages) add images to each section via another api
    - style sections view
    - style header footer

--- src/app/sections/list/list.js
+++ src/app/sections/list/list.js
@@ -1,15 +1,53 @@
 class SectionListController {
   /** @ngInject */
-  constructor($http, api) {
-    $http
-      .get(`http://content.guardianapis.com/sections?&api-key=${api.key}`)
-      .then(res => {
+  constructor($http, api, $q) {
+
+    function getSectionlist() {
+      return $http
+        .get(`http://content.guardianapis.com/sections?&api-key=${api.key}`)
+    }
+
+    const fetchImages = (res) => {
+      let imageCategories = res.data.response.results
+        .slice(0, 10)
+        .map((elem) => {
+          let endOfSubstring = elem.webTitle.indexOf(' ') !== -1 ? elem.webTitle.indexOf(' ') : elem.webTitle.length;
+          return elem.webTitle.substr(0, endOfSubstring);
+        })
+        .join('+');
+
+      imageCategories = 'buildings+news'; // @todo fix image requests;
+
+
+      let promises = {
+        'res': res,
+        'images': $http.get(`https://pixabay.com/api/?key=1630153-61e92123bad1886e631e44159&q=${imageCategories}&image_type=photo`).then(res => {return res})
+      }
+
+
+      return $q.all(promises);
+    }
+
+    const mapImagesToSections = ({articles, images}) => {
+
+      articles.map((article, articleIndex) => {
+        let imageIndex = articleIndex % images.length;
+        article.imageUrl = images[imageIndex].webformatURL;
+      });
+
+
+      return articles;
+    }
+
+    getSectionlist()
+      .then(fetchImages)
+      .then(promises => {
         let status;
 
-        status = res.status;
+        status = promises.res.status;
 
         if(status == 200) {
-          this.sections = res.data.response.results;
+          this.sections = mapImagesToSections({ articles: promises.res.data.response.results, images: promises.images.data.hits });
         }
       });
   }
